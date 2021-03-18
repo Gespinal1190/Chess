6dbf1868a0f861ff816509d7655c1a41e55d89fe
gespinal1190 <adalg.design@gmail.com>
2021-03-18 01:42:57 -0400

    js correo

--- js/bd-timer.js
+++ js/bd-timer.js
@@ -1,3 +1,13 @@
+
+// Initialize Cloud Firestore through Firebase
+
+firebase.initializeApp({
+    apiKey: '### FIREBASE API KEY ###',
+    authDomain: '### FIREBASE AUTH DOMAIN ###',
+    projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+  });
+  
+  var db = firebase.firestore();
 var endDate = new Date().getTime() + 1000800000;
 
 var x = setInterval(function() {
